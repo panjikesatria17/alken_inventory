a1969300ab4939e74dbf99f97fcdff35aec06f1e
panjikesatria17 <pancabudiman@gmail.com>
2023-09-20 04:48:01 +0700

    edit welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1019,7 +1019,7 @@
                   aria-controls="sidebarDashboards"
                 >
                   <i class="mdi mdi-speedometer"></i>
-                  <span data-key="t-dashboards">Dashboards</span>
+                  <span data-key="t-dashboards">Admin</span>
                 </a>
                 <div class="collapse menu-dropdown" id="sidebarDashboards">
                   <ul class="nav nav-sm flex-column">
@@ -1065,7 +1065,7 @@
                   aria-controls="sidebarApps"
                 >
                   <i class="mdi mdi-view-grid-plus-outline"></i>
-                  <span data-key="t-apps">Apps</span>
+                  <span data-key="t-apps">Gudang</span>
                 </a>
                 <div class="collapse menu-dropdown" id="sidebarApps">
                   <ul class="nav nav-sm flex-column">
@@ -1165,7 +1165,7 @@
                   aria-controls="sidebarLayouts"
                 >
                   <i class="mdi mdi-view-carousel-outline"></i>
-                  <span data-key="t-layouts">Layouts</span>
+                  <span data-key="t-layouts">Sales</span>
                   <span class="badge badge-pill bg-danger" data-key="t-hot"
                     >Hot</span
                   >
@@ -1184,6 +1184,44 @@
                   </ul>
                 </div>
               </li>
+
+              <li class="nav-item">
+                <a class="nav-link menu-link" href="#sidebarAuth" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="sidebarAuth">
+                    <i class="bx bx-user-circle"></i> <span data-key="t-authentication">Finance</span>
+                </a>
+                <div class="collapse menu-dropdown" id="sidebarAuth">
+                    <ul class="nav nav-sm flex-column">
+                        <li class="nav-item">
+                            <a href="#sidebarSignIn" class="nav-link" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="sidebarSignIn" data-key="t-signin"> Sign In
+                            </a>
+                            <div class="collapse menu-dropdown" id="sidebarSignIn">
+                                <ul class="nav nav-sm flex-column">
+                                    <li class="nav-item">
+                                        <a href="auth-signin-basic.html" class="nav-link" data-key="t-basic"> Basic </a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="auth-signin-cover.html" class="nav-link" data-key="t-cover"> Cover </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#sidebarSignUp" class="nav-link" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="sidebarSignUp" data-key="t-signup"> Sign Up
+                            </a>
+                            <div class="collapse menu-dropdown" id="sidebarSignUp">
+                                <ul class="nav nav-sm flex-column">
+                                    <li class="nav-item">
+                                        <a href="auth-signup-basic.html" class="nav-link" data-key="t-basic"> Basic </a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="auth-signup-cover.html" class="nav-link" data-key="t-cover"> Cover </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </li>
               <!-- end Dashboard Menu -->
           </div>
           <!-- Sidebar -->
